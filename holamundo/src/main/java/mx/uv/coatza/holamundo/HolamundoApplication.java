88bf7152610b43ff62a0c76f5997d22da3f0cd7c
José A. Vergara <jvergara99@gmail.com>
2024-04-03 15:57:08 -0600

    Primer mensaje en Spring - hula mundo

--- holamundo/src/main/java/mx/uv/coatza/holamundo/HolamundoApplication.java
+++ holamundo/src/main/java/mx/uv/coatza/holamundo/HolamundoApplication.java
@@ -1,13 +1,22 @@
 package mx.uv.coatza.holamundo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HolamundoApplication {
+public class HolamundoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HolamundoApplication.class, args);
 	}
+ 
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("Nuestro primer Spring...");
+		
+	}
+ 
 }
